81ab570aaa8b412420952afd584a7b97a30cbcd5
Sostenes Apollo <ctr.sostenes.dasilva@prth.com>
2024-08-05 22:20:44 -0300

    remove files and loading

--- app/lib/backup.ts
+++ app/lib/backup.ts
@@ -2,6 +2,8 @@ import { execSync } from 'child_process';
 import dayjs from 'dayjs';
 import { getBucketName, getDeviceName } from '~/routes/backup';
 import { uploadFile } from '~/routes/files';
+import fs from 'fs/promises';
+import path from 'path';
 
 export const backupDatabase = async () => {
   const DATABASE_URL = 'postgresql://postgres:postgres@localhost:5432/pedegas'
@@ -20,6 +22,28 @@ export const backupDatabase = async () => {
   }
 
   await uploadFile(path, bucket, filename);
+  await removeTarGzFiles();
 };
+
+async function removeTarGzFiles() {
+  try {
+    const directoryPath = process.cwd(); // Get the current working directory
+    const files = await fs.readdir(directoryPath);
+
+    
+    for (const file of files) {
+      const filePath = path.join(directoryPath, file);
+      console.log(directoryPath, file, path.extname(filePath));
+      if (path.extname(filePath) === '.gz') {
+        await fs.unlink(filePath);
+        console.log(`Deleted ${filePath}`);
+      }
+    }
+  } catch (error) {
+    console.error('Error removing .tar.gz files:', error);
+  }
+}
+
+removeTarGzFiles()
 
 // backupDatabase();
