f6c3ed7ffb6f01f45ed2c671e90c2c22bf662e60
Sostenes Apollo <ctr.sostenes.dasilva@prth.com>
2024-08-10 14:56:38 -0300

    .env.example update

--- app/routes/files.tsx
+++ app/routes/files.tsx
@@ -8,10 +8,10 @@ import fs from 'fs/promises';
 import { countRecords } from "~/lib/postgres";
 
 const s3 = new S3Client({
-  region: process.env?.AWS_REGION,
+  region: process.env?.STORAGE_REGION,
   credentials: {
-    accessKeyId: process.env?.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env?.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env?.STORAGE_ACCESS_KEY,
+    secretAccessKey: process.env?.STORAGE_SECRET,
   },
 } as any);
 
